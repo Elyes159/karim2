31c7a8a91378b1cb3d9615100073bda73fd8d362
Elyes159 <elyesmlik307@gmail.com>
2024-04-12 05:05:17 +0100

    Your commit message here

--- lib/FontSizeParam/settingFont.dart
+++ lib/FontSizeParam/settingFont.dart
@@ -1,13 +1,10 @@
 import 'dart:async';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:quran_app/features/home/pages/home_screen.dart';
 import 'package:quran_app/languages/languages.dart';
 import 'package:quran_app/languages/languages_constants.dart';
 import 'package:quran_app/main_view.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsPage extends StatefulWidget {
   final double initialFontSize;
@@ -19,14 +16,9 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
-  TextEditingController _fontSizeController = TextEditingController();
-
-  late double _fontSize;
-
   @override
   void initState() {
     super.initState();
-    _fontSize = widget.initialFontSize;
   }
 
   @override
@@ -81,60 +73,13 @@ class _SettingsPageState extends State<SettingsPage> {
           ),
         ],
       ),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Taille de la police',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            TextFormField(
-              controller: _fontSizeController, // Utilisez le contrôleur ici
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-
-              decoration: InputDecoration(
-                labelText: 'Taille de la police',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Text(
-              '$_fontSize',
-              style: TextStyle(fontSize: 20),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                String? userUID = await getUserUID();
-                if (userUID != null) {
-                  // Ajouter la valeur à Firestore avec le userUID comme identifiant de document
-                  // FirebaseFirestore.instance
-                  //     .collection('settings')
-                  //     .doc(userUID)
-                  //     .set({
-                  //   'fontSize': _fontSizeController.text,
-                  // });
-                  print("font size ajoutééééé");
-                } else {
-                  print(
-                      "FUIEZOIFEZEZNEZOINCOIEZNCOIEZNCOIENOIENOIECOIEZNCOIEZNOIEZCOIENOIENCOIEZNOIEZNCOIEZNCEZNOCIEZNC");
-                }
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) => HomeScreenNew()));
-              },
-              child: Text('Enregistrer'),
-            ),
-          ],
+          children: [],
         ),
       ),
     );
-  }
-
-  Future<void> _loadFontSize(double fontSize) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _fontSize = fontSize; // Utiliser la valeur passée comme argument
-      prefs.setDouble('fontSize', fontSize);
-    });
   }
 }
